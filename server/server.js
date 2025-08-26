868342d717c38fd3d7c4ecad80ac076288b736ac
Parz <jamwalparitosh@gmail.com>
2025-08-26 18:36:20 +0530

    feat: Initial project setup with Express and EJS structure

--- server/server.js
+++ server/server.js
@@ -1,19 +1,17 @@
-// Import the express library
 const express = require('express');
+const path = require('path'); // Import the 'path' module
 
-// Create an instance of an express application
 const app = express();
-
-// Define the port the server will run on
 const PORT = 3000;
 
-// Create a basic route for the homepage
-// When a user visits '/', it will send back a welcome message
-app.get('/', (req, res) => {
-    res.send('Welcome to the SkillSwap server!');
-});
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
 
-// Start the server and listen for incoming connections on the specified port
+// Tell Express where to find the views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// --- Routes will be added here later ---
+
 app.listen(PORT, () => {
     console.log(`Server is running successfully on http://localhost:${PORT}`);
 });
